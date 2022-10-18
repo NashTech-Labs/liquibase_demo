9e35e8d3f393a12ece58793905659bd2827684d4
Uzairrr1 <mohd.uzair@knoldus.com>
2022-10-18 14:56:43 +0530

    Initial commit

---
+++ src/main/resources/db/migration/V201708230744__INSERT_DATA2.sql
@@ -0,0 +1,13 @@
+ insert 
+    into
+        employee_table
+        (email, employee_name, salary) 
+    values
+        ('xyz.b@gmail.com', 'xyz', 90000.00);
+        
+ insert 
+    into
+        employee_table
+        (email, employee_name, salary) 
+    values
+        ('abc.b@gmail.com', 'abc', 560000.00);
